292bb9340462f8a2246c39840c1d983dd2d9e125
pechelleve <pechelleve@gmail.com>
2025-08-17 19:05:18 +0700

    Player rotation

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody _rigidBody;
     [SerializeField] private float _speed;
+    [SerializeField] private float _rotateSpeed;
     [SerializeField] private Transform _camera;
 
     private void Awake()
@@ -23,11 +25,17 @@ public class Player : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
         Vector3 horizontalDirection = horizontal * _camera.right;
         Vector3 verticalDirection = vertical * _camera.forward;
         verticalDirection.y = 0;
         horizontalDirection.y = 0;
         Vector3 movementDirection = horizontalDirection + verticalDirection;
+
+        transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * _rotateSpeed);
+        Vector3 playerDirection = transform.forward + _camera.forward;
+
         _rigidBody.velocity = movementDirection * _speed * Time.fixedDeltaTime;
+
     }
 }
