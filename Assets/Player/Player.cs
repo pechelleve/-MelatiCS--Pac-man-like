9debb51222683f8df8c4639b85c2f6d615266d09
pechelleve <pechelleve@gmail.com>
2025-08-17 10:26:11 +0700

    Player, Camera, Level, Pickable Coin

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _camera;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 horizontalDirection = horizontal * _camera.right;
+        Vector3 verticalDirection = vertical * _camera.forward;
+        verticalDirection.y = 0;
+        horizontalDirection.y = 0;
+        Vector3 movementDirection = horizontalDirection + verticalDirection;
+        _rigidBody.velocity = movementDirection * _speed * Time.fixedDeltaTime;
+    }
+}
